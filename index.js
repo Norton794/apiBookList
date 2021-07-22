81e5eaebcc9f086ccc957332b65d9d7c5d77882a
Norton dos Santos <nortonsantos79@gmail.com>
2021-07-22 16:38:55 -0300

    Permiti apenas as operacoes GET, POST, PUT, DELETE

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "http://localhost:8083/");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE ");
     app.use(cors());
     next();
 });
